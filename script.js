275f0ebd58674915bdb156a06f41f36b5390c9cd
bazhonglbj <bazhonglbj@gmail.com>
2023-09-21 20:18:51 -0700

    bug fix lol

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ var photos = [];
 var fileNames = []; 
 var imageList = []; 
 var image ; 
-var openList = "<li id='photo'>";
+var openList = "<li class='photo'>";
 var closeList = "</li>"; 
 var descriptions = [
-      "You're such a little angel!",
+      "You are such a little angel!",
       "My heart skips a beat every time I see you.",
-      "You're the cutest little thing!",
-      "You're always so well-behaved, good job!",
+      "You are the cutest little thing!",
+      "You are always so well-behaved, good job!",
       "Who could resist those gentle eyes?",
-      "You're the little superstar of the pet world!",
+      "You are the little superstar of the pet world!",
       "Your furry appearance is just so captivating!",
-      "You're always so full of energy.",
-      "You're the best companion.",
+      "You are always so full of energy.",
+      "You are the best companion.",
       "You bring endless joy every day!",
       "What a cutie cat!",
       "I will love you always!",
@@ -33,7 +33,6 @@ for(var i = 0; i < 12 ; i++){
     imageList.push(image); 
 }
 function showInfoBox(caption, description) {
-    console.log("showInfoBox triggered", caption, description);
     document.getElementById("infoBoxCaption").innerText = caption;
     document.getElementById("infoBoxDescription").innerText = description;
     document.getElementById("infoBox").style.display = "block";
